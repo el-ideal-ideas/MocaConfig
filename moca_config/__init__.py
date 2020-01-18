e6d5a829326385a0c44a999ac32445bdf0cfe3b1
el.ideal-ideas <el.idealideas@gmail.com>
2020-01-19 00:21:13 +0900

    Use new line mark

--- moca_config/__init__.py
+++ moca_config/__init__.py
@@ -1,3 +1,35 @@
+# Ω*
+#               ■          ■■■■■  
+#               ■         ■■   ■■ 
+#               ■        ■■     ■ 
+#               ■        ■■       
+#     ■■■■■     ■        ■■■      
+#    ■■   ■■    ■         ■■■     
+#   ■■     ■■   ■          ■■■■   
+#   ■■     ■■   ■            ■■■■ 
+#   ■■■■■■■■■   ■              ■■■
+#   ■■          ■               ■■
+#   ■■          ■               ■■
+#   ■■     ■    ■        ■■     ■■
+#    ■■   ■■    ■   ■■■  ■■■   ■■ 
+#     ■■■■■     ■   ■■■    ■■■■■
+
+
+"""
+Moca Config Module
+モカシステム設定モジュール
+茉客系统设定模块
+
+[MocaConfig]
+
+Copyright (c) 2020.1.17 [el.ideal-ideas]
+
+This software is released under the MIT License. see LICENSE.txt.
+
+https://www.el-ideal-ideas.com
+"""
+
+
 from .MocaConfig import MocaConfig, MocaFileError, config_routes_v1, run_server, VERSION
 
 __copyright__ = 'Copyright (C) 2020.1.17 <el.ideal-ideas: https://www.el-ideal-ideas.com>'
